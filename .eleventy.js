c70c689aabcb9ea15a979be35d56a5e83886b60f
gayunn <belllaxix@gmail.com>
2024-07-19 17:17:23 +0900

    WIP : 인트로

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,28 @@
+const sass = require("sass");
+const path = require("path");
+
 module.exports = function(eleventyConfig) {
-    return {
-      dir: {
-        input: "src",
-        output: "dist",
-        includes: "_includes"  
-      }
-    };
+  eleventyConfig.addTemplateFormats("scss");
+
+  // Compile Sass to CSS
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    compile: async function(inputContent, inputPath) {
+      let result = sass.compile(inputPath);
+      return async (data) => {
+        return result.css;
+      };
+    }
+  });
+
+  // CSS 파일을 출력 디렉토리로 복사
+  eleventyConfig.addPassthroughCopy("styles");
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      includes: "_includes"
+    }
   };
+};
